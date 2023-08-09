dc031b58e76c72fe67a65e3285350b30928d223e
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-08 21:20:54 -0300

    Criando o projeto maven Spring

---
+++ src/test/java/com/daniel/crudspringangular/CrudSpringAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.crudspringangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
