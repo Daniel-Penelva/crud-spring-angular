49b11445e523a3c51e8f6ad150828af511757ec1
Daniel Penelva <d4n.andrade@gmail.com>
2023-08-08 22:54:20 -0300

    Configuração maven e Hello World com Spring

--- src/test/java/com/daniel/crudspringangular/CrudSpringAngularApplicationTests.java
+++ src/test/java/com/daniel/crudspringangular/CrudSpringAngularApplicationTests.java
@@ -1,12 +1,11 @@
 package com.daniel.crudspringangular;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CrudSpringAngularApplicationTests {
 
-	@Test
+	@org.junit.Test
 	void contextLoads() {
 	}
 
