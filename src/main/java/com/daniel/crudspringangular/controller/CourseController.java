89524baae64bb04e8db8ec67ebd97be793b9e54b
Daniel Penelva <d4n.andrade@gmail.com>
2023-09-05 20:17:57 -0300

    Criando método buscar por ID (controller)

--- src/main/java/com/daniel/crudspringangular/controller/CourseController.java
+++ src/main/java/com/daniel/crudspringangular/controller/CourseController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,19 +38,27 @@ public class CourseController {
      * Ou pode usar o lombok com a anotação: @AllArgsConstructor
     */
     
+    // Listar curso - http://localhost:8080/api/courses
     // @RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public @ResponseBody List<Course> list(){
        return courseRepository.findAll();
     }
 
+    // Criar curso - http://localhost:8080/api/courses
     // @RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course){
-        //System.out.println(course.getName());
-       //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course)); <Este exemplo declara o tipo ResponseEntity<Course>>
-
+       
        return courseRepository.save(course);
+    }
+
+    // Buscar curso por id - http://localhost:8080/api/courses/{id}
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
     }
 }
