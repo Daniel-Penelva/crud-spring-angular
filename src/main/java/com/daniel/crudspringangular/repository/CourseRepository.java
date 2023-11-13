f3c77c3295a8b6dd3b0aeca7601559c8734ea443
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-13 20:22:41 -0300

    Atualizando para Spring 3 e versão do java 17

--- src/main/java/com/daniel/crudspringangular/repository/CourseRepository.java
+++ src/main/java/com/daniel/crudspringangular/repository/CourseRepository.java
@@ -1,10 +1,8 @@
 package com.daniel.crudspringangular.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.daniel.crudspringangular.model.Course;
 
-@Repository
 public interface CourseRepository extends JpaRepository<Course, Long>{
 }
