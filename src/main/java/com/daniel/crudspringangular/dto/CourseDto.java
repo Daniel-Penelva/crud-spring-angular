9f3fcb641d4242d2d1db8737331888906811b89c
Daniel Penelva <d4n.andrade@gmail.com>
2024-05-24 20:33:35 -0300

    Criando validando Enums com Java Bean Validations

--- src/main/java/com/daniel/crudspringangular/dto/CourseDto.java
+++ src/main/java/com/daniel/crudspringangular/dto/CourseDto.java
@@ -3,18 +3,20 @@ package com.daniel.crudspringangular.dto;
 import java.util.List;
 
 import org.hibernate.validator.constraints.Length;
+
+import com.daniel.crudspringangular.enums.Category;
+import com.daniel.crudspringangular.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDto(
     @JsonProperty("_id") Long id, 
     @NotBlank @NotNull @Length(min = 3, max = 100) String name, 
-    @NotNull @Length(max = 10) @Pattern(regexp = "Back-end|Front-end") String category, 
+    @NotNull @Length(max = 10) @ValueOfEnum(enumClass = Category.class) String category, 
     @NotNull @NotEmpty @Valid List<LessonDto> lessons) {
     
 }
---
+++ src/main/java/com/daniel/crudspringangular/enums/validation/ValueOfEnumValidator.java
@@ -0,0 +1,30 @@
+package com.daniel.crudspringangular.enums.validation;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
+    private List<String> acceptedValues;
+
+    @Override
+    public void initialize(ValueOfEnum annotation) {
+        acceptedValues = Stream.of(annotation.enumClass().getEnumConstants())
+                .map(Enum::toString)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true;
+        }
+
+        return acceptedValues.contains(value.toString());
+    }
+}
+
+// Fonte dessa interface generica - Exemplo Validation 5: https://www.baeldung.com/javax-validations-enums
