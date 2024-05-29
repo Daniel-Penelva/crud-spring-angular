8a7f9ab4b9eed44b64b5b224834dc28915457b35
Daniel Penelva <d4n.andrade@gmail.com>
2024-05-28 21:47:07 -0300

    Spring Docker Compose (exemplo2) - alterando docker compose + criando os arquivos de configuração de BD + deletando a biblioteca docker compose

--- src/main/java/com/daniel/crudspringangular/CrudSpringAngularApplication.java
+++ src/main/java/com/daniel/crudspringangular/CrudSpringAngularApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import com.daniel.crudspringangular.enums.Category;
 import com.daniel.crudspringangular.model.Course;
@@ -19,6 +20,7 @@ public class CrudSpringAngularApplication {
 
 	// Para testar a lista de cursos
 	@Bean
+	@Profile("dev")
 	CommandLineRunner initDataBase(CourseRepository courseRepository){
 		return args ->{
 			courseRepository.deleteAll();
